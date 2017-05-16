030da9257e7c855e7bb37537fffc65d46e1721c2
AKosmachyov <a.kosmachyov@hotmail.com>
2017-05-16 13:51:12 +0300

    Added elements to the header

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,12 +6,24 @@ import { NewsService } from './services/news-service';
 @Component({
     selector: 'app',
     template: `
-        <header>header</header>
+        <header>
+            <div class="container normalize-height">
+                <div class="row normalize-height">
+                    <div class="col-md-4 col-xs-4">
+                        logo
+                    </div>
+                    <div class="col-md-4  col-md-offset-4 col-xs-6 normalize-height">
+                        <button class="btn btn-primary">Вход</button>                
+                        <button class="btn btn-primary">Регистрация</button>
+                    </div>
+                </div>
+            </div>
+        </header>
         <div class="container">
-            <div class="col-md-9">
+            <div class="col-md-8 col-xs-8">
                 <preview-news *ngFor="let item of news" [news]="item"></preview-news>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-4 col-xs-4">
                 search element
             </div>
         </div>                
@@ -21,6 +33,9 @@ import { NewsService } from './services/news-service';
             height: 68px;
             border-bottom: 1px solid #d5dddf;
             background: #fff;
+        }
+        .normalize-height {
+            height: inherit;
         }
     `]
 })
